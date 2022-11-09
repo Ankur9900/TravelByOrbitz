5caedbd32a35bbcb78a04f6a516c0f8c2e2aa289
sanjayrathva091 <sanjayrathva091@gmail.com>
2022-11-09 19:11:10 +0530

    ADDED: hotel page

---
+++ orbitz/src/Pages/Hotes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FeaturedOffers from "../Components/FeaturedOffers";
+import WhatsNew from "../Components/WhatsNew";
+
+function Hotels() {
+  return (
+    <div>
+      <WhatsNew />
+      <FeaturedOffers />
+    </div>
+  );
+}
+
+export default Hotels;

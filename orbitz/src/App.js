d4fc67ecf70743204f4c79f94941bba8be3e1446
Ankur9900 <arya.ankur99@gmail.com>
2022-11-11 23:56:03 +0530

    3rd day done

--- orbitz/src/App.js
+++ orbitz/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import HelpBot from './Components/helpbot/HelpBot';
 import AllRoutes from './Routes/AllRoutes';
 
 function App() {
   return (
     <div className="App">
     <AllRoutes/>
+    <HelpBot/>
     </div>
   );
 }
